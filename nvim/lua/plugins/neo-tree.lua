49e58e340401566fc484c3fcb3d742e7a92728f8
Kori <kori.hanes@sev1tech.com>
2025-10-01 09:43:33 -0400

    neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -6,6 +6,16 @@ return {
   "MunifTanjim/nui.nvim",
   "nvim-tree/nvim-web-devicons", 
   },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                hide_hidden = false,
+            },
+        },
+    },
   config = function()
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
   end
